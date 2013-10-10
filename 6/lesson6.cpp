3d8448eeb23ddd7c6e7ba6e9192ed8e3a08547a9
Matt Hinze <mhinze@gmail.com>
2013-10-10 07:50:41 -0500

    finished lesson6

--- 6/lesson6.cpp
+++ 6/lesson6.cpp
@@ -6,10 +6,10 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 
-SDL_Surface *foo = NULL;
-SDL_Surface *background = NULL;
+SDL_Surface *dots = NULL;
 SDL_Surface *screen = NULL;
 SDL_Event event;
+SDL_Rect clip[4];
 
 SDL_Surface *load_image(std::string filename)
 {
@@ -26,12 +26,12 @@ SDL_Surface *load_image(std::string filename)
    return optimizedImage;
 }
 
-void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination)
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL)
 {
    SDL_Rect offset;
    offset.x = x;
    offset.y = y;
-   SDL_BlitSurface(source, NULL, destination, &offset);
+   SDL_BlitSurface(source, clip, destination, &offset);
 }
 
 bool init()
@@ -55,13 +55,8 @@ bool init()
 
 bool load_files()
 {
-   foo = load_image("foo.png");
-   background = load_image("background.png");
-   if (foo == NULL)
-   {
-      return false;
-   }
-   if (background == NULL)
+   dots = load_image("dots.png");
+   if (dots == NULL)
    {
       return false;
    }
@@ -70,8 +65,7 @@ bool load_files()
 
 void clean_up()
 {
-   SDL_FreeSurface(foo);
-   SDL_FreeSurface(background);
+   SDL_FreeSurface(dots);
    SDL_Quit();
 }
 
@@ -89,8 +83,32 @@ int main(int argc, char* args[])
       return 1;
    }
    
-   apply_surface(0, 0, background, screen);
-   apply_surface(240, 190, foo, screen);
+   clip[0].x = 0;
+   clip[0].y = 0;
+   clip[0].w = 100;
+   clip[0].h = 100;
+   
+   clip[1].x = 100;
+   clip[1].y = 0;
+   clip[1].w = 100;
+   clip[1].h = 100;
+   
+   clip[2].x = 0;
+   clip[2].y = 100;
+   clip[2].w = 100;
+   clip[2].h = 100;
+
+   clip[3].x = 100;
+   clip[3].y = 100;
+   clip[3].w = 100;
+   clip[3].h = 100;
+   
+   SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+
+   apply_surface(0, 0, dots, screen, &clip[0]);
+   apply_surface(540, 0, dots, screen, &clip[1]);
+   apply_surface(0, 380, dots, screen, &clip[2]);
+   apply_surface(540, 380, dots, screen, &clip[3]);
 
    if (SDL_Flip(screen) == -1)
    {
